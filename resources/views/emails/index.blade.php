dd95a2ce39d1e007e9d9563aec567e7a788ef7aa
Rihards Kivulans <ext.rihards@kecom.lv>
2024-08-12 13:44:23 +0300

    WIP: json dati.

--- resources/views/emails/index.blade.php
+++ resources/views/emails/index.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <h1>Mailboxes</h1>
-
-
+        
+        @foreach ( $users as $user )
+           {{ $user->name }} 
+        @endforeach
         
     </div>
 </x-app-layout>
