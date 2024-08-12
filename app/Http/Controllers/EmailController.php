1730b84fe9b07962f1552db4f5580e81870b9944
Lauris <ext.lauris@kecom.lv>
2024-08-12 14:39:26 +0300

    emailController commited

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,32 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class EmailController extends Controller
 {
-  public function index()
-  {
+    public function index()
+    {
 
-    $dati = json_decode(
-      <<<JSON
-        [
-          { "email" : "bills@kecom.lv", "privileges": [ ], "status": "active" },
-          { "email" : "ext.lauris@kecom.lv", "privileges": [ ], "status": "active" }, 
-          { "email" : "ext.rihards@kecom.lv", "privileges": [ ], "status": "active" },
-          { "email" : "pve@kecom.lv", "privileges": [ ], "status": "active" },
-          { "email" : "gitea@kecom.lv", "privileges": [ ], "status": "active" },
-          { "email" : "kristaps@kecom.lv", "privileges": [ "admin" ], "status": "active" },
-          { "email" : "leadcollect@kecom.lv", "privileges": [ ], "status": "active" },
-          { "email" : "odoo@kecom.lv", "privileges": [ ], "status": "active" },
-          { "email" : "passit@kecom.lv", "privileges": [ ], "status": "active" }
-        ]
-        JSON
-    );
+        $dati = json_decode(<<<'JSON'
+            [
+                { "email" : "bills@kecom.lv", "privileges": [ ], "status": "active" },
+                { "email" : "ext.lauris@kecom.lv", "privileges": [ ], "status": "active" }, 
+                { "email" : "ext.rihards@kecom.lv", "privileges": [ ], "status": "active" },
+                { "email" : "pve@kecom.lv", "privileges": [ ], "status": "active" },
+                { "email" : "gitea@kecom.lv", "privileges": [ ], "status": "active" },
+                { "email" : "kristaps@kecom.lv", "privileges": [ "admin" ], "status": "active" },
+                { "email" : "leadcollect@kecom.lv", "privileges": [ ], "status": "active" },
+                { "email" : "odoo@kecom.lv", "privileges": [ ], "status": "active" },
+                { "email" : "passit@kecom.lv", "privileges": [ ], "status": "active" }
+            ]
+        JSON);
 
-
-    return view('emails.index', [
-      'users' => $dati
-    ]);
-  }
+        return view('emails.index', [
+            'users' => $dati,
+        ]);
+    }
 }
