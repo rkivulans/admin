155520af69f769a25a6509892f5f4e0b0662dbb5
Lauris <ext.lauris@kecom.lv>
2024-08-13 13:43:28 +0300

    WIP: added aliases

--- app/Http/Controllers/AliasController.php
+++ app/Http/Controllers/AliasController.php
@@ -2,13 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class AliasController extends Controller
 {
     public function index()
     {
 
+        $dati = json_decode(<<<'JSON'
+             [
+                { "address": "user1@example.com", "address_display": "user1@example.com", "forwards_to": [ "user@example.com" ], "permitted_senders": [ "user@example.com" ], "required": true } ,
+                { "address": "user2@example.com", "address_display": "user2@example.com", "forwards_to": [ "user@example.com", "user@example.com" ], "permitted_senders": [ "user@example.com", "user@example.com" ], "required": true } ,
+                { "address": "user3@example.com", "address_display": "user3@example.com", "forwards_to": [ "user@example.com" ], "permitted_senders": [ "user@example.com" ], "required": false } ,
+                { "address": "user4@example.com", "address_display": "user4@example.com", "forwards_to": [ "user@example.com", "user@example.com"  ], "permitted_senders": [ "user@example.com", "user@example.com" ], "required": false} ,
+                { "address": "user5@example.com", "address_display": "user5@example.com", "forwards_to": [ "user@example.com" ], "permitted_senders": [ "user@example.com" ], "required": false} 
+             ]
+        JSON);
+
+        
+
         return view('aliases.index', [
-            'alias' => 'Lauris TEST alias',
+            'aliases' => $dati,
+        ]);
+    }
+
+
+    public function create()
+    {
+
+        return view('aliases.create', []);
+    }
+
+    public function edit(Request $request)
+    {
+
+        $email = 'kristaps321@example.coom';
+
+        return view('aliases.edit', [
+            'email' => $email,
+
         ]);
     }
 }
--- resources/views/aliases/index.blade.php
+++ resources/views/aliases/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <h1>Aliases</h1>
-        
+        @dump($aliases)
     </div>
 </x-app-layout>
