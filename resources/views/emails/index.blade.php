42553499ddf2f861e3b379a712d015107a616877
Lauris <ext.lauris@kecom.lv>
2024-08-13 10:01:23 +0300

    table update

--- resources/views/emails/index.blade.php
+++ resources/views/emails/index.blade.php
@@ -1,8 +1,6 @@
 <x-app-layout>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="prose">
-            <h1>Mailboxes</h1>
-        </div>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">  
+        <h1 class="text-lg">Mailboxes</h1>
         <div class="mt-8 flow-root">
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
@@ -24,7 +22,7 @@
                                 @foreach ($users as $user)
                                     <tr>
                                         <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                                            {{ $user->email }}
+                                            {{ $user->name }}
                                         </td>
                                         <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                                             <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only"></span></a>
